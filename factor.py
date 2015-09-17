660b8ab43ac673a0fdab45d1f202fbee00323299
George Hotz <geohot@gmail.com>
2015-09-17 19:37:57 +0000

    subset product is swag

---
+++ factor.py
@@ -0,0 +1,38 @@
+import sys
+import random
+from fractions import gcd
+
+def factor(n):
+  ret = []
+  nn = n
+
+  phi = n
+
+  d = 2
+  while nn > 1:
+    done = False
+    while nn%d == 0:
+      if not done:
+        phi = phi - (phi/d)
+        done = True
+      print d,
+      ret.append(d)
+      sys.stdout.flush()
+      nn /= d
+    d += 1
+
+  print ""
+  print "phi: ", phi
+    
+  for i in range(30):
+    rr = random.randint(1, n)
+    rr = rr/gcd(rr, n)
+    print pow(rr, phi, n),
+  print ""
+
+  return ret, phi
+
+if __name__ == "__main__":
+  n = int(sys.argv[1])
+  factor(n)
+
