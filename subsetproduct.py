135b640876d80eadf700fa540fd2c5c1a8f73190
George Hotz <geohot@gmail.com>
2015-09-22 12:40:53 -0700

    z3 go faster come on bro

--- subsetproduct.py
+++ subsetproduct.py
@@ -5,7 +5,7 @@ import gmpy
 
 primes = filter(gmpy.is_prime, range(1000))
 
-P = primes[20:28]
+P = primes[20:40]
 print P
 print "2^%d" % len(P)
 
@@ -14,8 +14,15 @@ print "finding", N
 
 ret, phi = factor(N)
 
-print "2^%f" % (log(phi)/log(2))
+odds = log(phi)/log(2)
+print "2^%f" % odds
 
+print "will find",len(P)-odds
+
+if len(P)-odds < 1.0:
+  exit(-1)
+
+"""
 # brute force
 for i in xrange(1 << len(P)):
   # check, but don't add it to choice
@@ -39,6 +46,7 @@ for i in xrange(1 << len(P)):
     # x*N === -1 mod p
     for p in choice:
       print p, x*N % p
+"""
 
 # we know the target number is === 1 mod <all factors>
 #                          and === 0 mod <all things used in subset>
@@ -59,20 +67,29 @@ for x in rret:
 mat = np.asarray(mat).astype(np.int).T
 print mat
 
+def generator(x):
+  fact, phi = factor(x-1)
+  #g = sorted(fact)[::-1][0]
+  return fact
+
 # mserrano: there's an isomorphism between (Z/pZ)^* and (Z/(p-1)Z)^+ for odd primes p
 def isomorphism(x):
-  quack = {1: 0}
 
-  # get generator
-  if x == 7:
-    g = 3
-  if x == 5:
-    g = 2
-  if x == 3:
-    g = 2
+  # get potential generators
+  #G, phi = factor(x-1)
 
-  for i in range(1, x-1):
-    quack[(g**i) % x] = i
+  #for g in G:
+  for g in range(2, x):
+    quack = {1: 0}
+    for i in range(1, x-1):
+      quack[(g**i) % x] = i
+    if len(quack) == x-1:
+      print x,"has generator",g
+      break
+
+  if len(quack) != x-1:
+    print "FAILED FAILED FAILED", x, G
+    exit(-2)
 
   return quack
 
@@ -86,9 +103,11 @@ print pmat
 
 #test = [1,0,0,0,1,1,1,1]
 
+"""
 test = [1,1,0,1,1,0,1,1]
 print test
 print np.dot(test, pmat), re, np.dot(test, pmat) % np.asarray(re)
+"""
 
 #exit(0)
 
@@ -96,28 +115,34 @@ print np.dot(test, pmat), re, np.dot(test, pmat) % np.asarray(re)
 
 # for example, x*y === 1 mod 3, how do we know?
 
+print "WE ARE IMPORTING Z3"
 from z3 import *
 
 s = Solver()
 
 bb = []
 for i in range(pmat.shape[0]):
-  b = Int('b_'+str(i))
-  s.add(b >= 0)
-  s.add(b <= 1)
+  #b = Int('b_'+str(i))
+  #s.add(b >= 0)
+  #s.add(b <= 1)
+  b = Bool('b_'+str(i))
   if i == 0:
-    bsum = b
+    bor = b
   else:
-    bsum += b
+    bor = Or(b, bor)
   bb.append(b)
-s.add(bsum != 0)
+
+# at least one must be True
+s.add(bor == True)
 
 for j in range(pmat.shape[1]):
   for i in range(pmat.shape[0]):
     if i == 0:
-      ss = bb[i] * pmat[i,j]
+      #ss = bb[i] * pmat[i,j]
+      ss = If(bb[i], pmat[i,j], 0)
     else:
-      ss += bb[i] * pmat[i,j]
+      #ss += bb[i] * pmat[i,j]
+      ss += If(bb[i], pmat[i,j], 0)
   s.add(ss % re[j] == 0)
   
 print s.check()
